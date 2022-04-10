92b096ccb46c0fce63b461c96e150c94b7fa26d3
ventaclashroyale2020 <ventaclashroyale2020@gmail.com>
2022-04-09 18:35:56 -0600

    Add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterUsersComponent } from './components/register-users/register-users.component';
+import { AddCarsComponent } from './components/add-cars/add-cars.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterUsersComponent,
+    AddCarsComponent
   ],
   imports: [
     BrowserModule,
